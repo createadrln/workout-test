0e2537e3b38c76538200b33ead8064c1c72aca6a
Ryan Mac <ryan@createadrenaline.com>
2015-08-06 08:45:59 -0400

    bug fixes

--- app/exercise/scripts/EditController.js
+++ app/exercise/scripts/EditController.js
@@ -7,7 +7,8 @@ angular
         $scope.selectedWeightUnit = $scope.exercise.weight_unit;
         $scope.weight_units = [
             { 'id' : 1, 'unit' : 'lbs' },
-            { 'id' : 2, 'unit' : 'kgs' }
+            { 'id' : 2, 'unit' : 'kgs' },
+            { 'id' : 3, 'unit' : 'None' }
         ];
         $scope.exercise.weight_unit = getIndexOfId($scope.weight_units, $scope.selectedWeightUnit.id);
 
