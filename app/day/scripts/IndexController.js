627479d2dc70d9308bc8460c1bca6648537c8b7c
Ryan Mac <ryan@createadrenaline.com>
2015-09-11 16:26:29 -0400

    data storage backup to sqllite

--- app/day/scripts/IndexController.js
+++ app/day/scripts/IndexController.js
@@ -3,104 +3,96 @@ angular
     .controller("IndexController", function ($scope, $localStorage, supersonic) {
         $scope.showSpinner = true;
 
-        $scope.initializeLocalDays = [
-            {
-                'id' : generateUUID(),
-                'day' : 'Sunday',
-                'brief_description' : '',
-                'order' : 1,
-                'workouts' : []
-            },
-            {
-                'id' : generateUUID(),
-                'day' : 'Monday',
-                'brief_description' : '',
-                'order' : 2,
-                'workouts' : []
-            },
-            {
-                'id' : generateUUID(),
-                'day' : 'Tuesday',
-                'brief_description' : '',
-                'order' : 3,
-                'workouts' : []
-            },
-            {
-                'id' : generateUUID(),
-                'day' : 'Wednesday',
-                'brief_description' : '',
-                'order' : 4,
-                'workouts' : []
-            },
-            {
-                'id' : generateUUID(),
-                'day' : 'Thursday',
-                'brief_description' : '',
-                'order' : 5,
-                'workouts' : []
-            },
-            {
-                'id' : generateUUID(),
-                'day' : 'Friday',
-                'brief_description' : '',
-                'order' : 6,
-                'workouts' : []
-            },
-            {
-                'id' : generateUUID(),
-                'day' : 'Saturday',
-                'brief_description' : '',
-                'order' : 7,
-                'workouts' : []
+        /*SQL LITE STORAGE FOR USER DATA*/
+        document.addEventListener("deviceready", onDeviceReady, false);
+        function onDeviceReady() {
+
+            var flexFileDb = window.sqlitePlugin.openDatabase({name: "flexfile.data", createFromLocation: 1});  // Database Var //
+            if (!$localStorage.localDays && !$localStorage.localExercises && !$localStorage.localWorkouts) { // If No Days or Exercises or Workouts //
+                if (flexFileDb.localDays || flexFileDb.localExercises || flexFileDb.localWorkouts) {  // If SQL Exercises or Workouts Exist //
+                    if (!$localStorage.localDays && flexFileDb.localDays) {  // If No Local Days and SQL Days Exist //
+                        $localStorage.localDays = flexFileDb.localDays;  // Local Workouts equals SQL Workouts //
+                        $scope.localDays = $localStorage.localDays;
+                    }
+                    if (!$localStorage.localWorkouts && flexFileDb.localWorkouts) {  // If No Local Workouts and SQL Workouts Exist //
+                        $localStorage.localWorkouts = flexFileDb.localWorkouts;  // Local Workouts equals SQL Workouts //
+                    }
+                    if (!$localStorage.localExercises && flexFileDb.localExercises) {  // If No Local Exercises and SQL Exercises Exist //
+                        $localStorage.localExercises = flexFileDb.localExercises;  // Local Exercises equals SQL Exercises //
+                    }
+                } else {  // If SQL Exercises or Workouts Do Not Exist //
+                    $scope.initializeLocalDays = [
+                        {
+                            'id': generateUUID(),
+                            'day': 'Sunday',
+                            'brief_description': '',
+                            'order': 1,
+                            'workouts': []
+                        },
+                        {
+                            'id': generateUUID(),
+                            'day': 'Monday',
+                            'brief_description': '',
+                            'order': 2,
+                            'workouts': []
+                        },
+                        {
+                            'id': generateUUID(),
+                            'day': 'Tuesday',
+                            'brief_description': '',
+                            'order': 3,
+                            'workouts': []
+                        },
+                        {
+                            'id': generateUUID(),
+                            'day': 'Wednesday',
+                            'brief_description': '',
+                            'order': 4,
+                            'workouts': []
+                        },
+                        {
+                            'id': generateUUID(),
+                            'day': 'Thursday',
+                            'brief_description': '',
+                            'order': 5,
+                            'workouts': []
+                        },
+                        {
+                            'id': generateUUID(),
+                            'day': 'Friday',
+                            'brief_description': '',
+                            'order': 6,
+                            'workouts': []
+                        },
+                        {
+                            'id': generateUUID(),
+                            'day': 'Saturday',
+                            'brief_description': '',
+                            'order': 7,
+                            'workouts': []
+                        }
+                    ];
+                    $localStorage.localDays = $scope.initializeLocalDays;  // Initialize Days //
+                    $scope.localDays = $localStorage.localDays;  // Initialize Days //
+                    flexFileDb.localDays = $localStorage.localDays;  // Save Local To DB //
+                    flexFileDb.localWorkouts = $localStorage.localWorkouts;  // Save Local To DB //
+                    flexFileDb.localExercises = $localStorage.localExercises;  // Save Local To DB //
+                }
+            } else { // If Local Exercises or Workouts //
+                $scope.localDays = $localStorage.localDays;
+                flexFileDb.localDays = $localStorage.localDays;  // Save Local To DB //
+                flexFileDb.localWorkouts = $localStorage.localWorkouts;  // Save Local To DB //
+                flexFileDb.localExercises = $localStorage.localExercises;  // Save Local To DB //
             }
-        ];
 
-        $scope.initializeLocalAutocompleteTags = [
-            { text: 'Chest' },
-            { text: 'Back' },
-            { text: 'Legs' },
-            { text: 'Core' },
-            { text: 'Arms' },
-            { text: 'Shoulders' },
-            { text: 'Calves' },
-            { text: 'Quads' },
-            { text: 'Lats' },
-            { text: 'Delts' },
-            { text: 'Hamstrings' },
-            { text: 'Traps' },
-            { text: 'Forearms' }
-        ];
-
-        if ($localStorage.localDays) {
-            $scope.localDays = $localStorage.localDays;
-        } else {
-            $localStorage.localDays = $scope.initializeLocalDays;
-            $scope.localDays = $localStorage.localDays;
-        }
-
-        if ($localStorage.tags) {
-            $scope.tags = $localStorage.tags;
-        } else {
-            $localStorage.tags = $scope.initializeLocalTags;
-        }
-
-        supersonic.data.channel('localDays').subscribe( function(localDays) {
-            $scope.$apply(function() {
-                $scope.localDays = localDays;
+            supersonic.data.channel('localDays').subscribe(function (localDays) {
+                $scope.$apply(function () {
+                    $scope.localDays = localDays;
+                });
             });
-        });
 
-        $scope.localDays = $localStorage.localDays;
-        $scope.showSpinner = false;
+            $scope.showSpinner = false;
 
-        var menuBtn = new supersonic.ui.NavigationBarButton({
-            title: 'info',
-            styleId: "info"
-        });
-
-        supersonic.ui.navigationBar.update({
-            overrideBackButton: false
-        }).then(supersonic.ui.navigationBar.show());
-
+        }
 
     });
