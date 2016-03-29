48b93c72f2b1c0c74099649c914b6f2f2f5bc959
createadrln <ryan@createadrenaline.com>
2016-03-29 06:15:17 -0400

    set series data list

--- app/exercise/scripts/NewExerciseStep3Controller.js
+++ app/exercise/scripts/NewExerciseStep3Controller.js
@@ -3,60 +3,69 @@ angular
     .controller("NewExerciseStep3Controller", function ($scope, $localStorage, supersonic) {
 
         $scope.exercise = $localStorage.newExercise;
+
+        //Rep Fixed Settings and Functions//
+
         $scope.exercise.reps_fixed = 1;
-        $scope.exercise.rep_range_start = 1;
-        $scope.exercise.rep_range_end = 1;
-        
-        /*Rep Fixed Toggles*/
+
         $scope.increaseFixedRepQty = function() {
             $scope.exercise.reps_fixed++;
-        }
+        };
 
         $scope.decreaseFixedRepQty = function() {
             $scope.exercise.reps_fixed--;
-        }
+        };
 
-        /*Rep Range Toggles*/
+        //Rep Range Settings and Functions//
+
+        $scope.exercise.rep_range_start = 1;
+        $scope.exercise.rep_range_end = 1;
+
         $scope.increaseRepRangeStartQty = function() {
             $scope.exercise.rep_range_start++;
-        }
+        };
 
         $scope.decreaseRepRangeStartQty = function() {
             $scope.exercise.rep_range_start--;
-        }
+        };
 
         $scope.increaseRepRangeEndQty = function() {
             $scope.exercise.rep_range_end++;
-        }
+        };
 
         $scope.decreaseRepRangeEndQty = function() {
             $scope.exercise.rep_range_end--;
+        };
+
+        //Rep Series Settings and Functions//
+
+        $scope.exercise.reps_series = {};
+        for(var set = 0; set < $scope.exercise.sets; set++ ) {
+            $scope.exercise.reps_series['reps_series' + '_' + set] = 1;
         }
 
-        /*Rep Series Toggles*/
         $scope.getCount = function(num) {
             return new Array(num);   
-        }
-
-        for(set = 0; set < $scope.exercise.sets; set++ ) {
-            // $scope.exercise.rep_series_ + parseInt(set) = 1;
-        }
+        };
 
         $scope.increaseRepSeriesQty = function($index) {
-            // $scope.exercise.rep_series_+$index++;
-        }
+            $scope.exercise.reps_series['reps_series' + '_' + $index]++;
+        };
 
         $scope.decreaseRepSeriesQty = function($index) {
-    		// $scope.exercise.rep_series_+$index--;
-        }
-    
-        $scope.addToExerciseStep3 = function() {
+            $scope.exercise.reps_series['reps_series' + '_' + $index]++;
+        };
 
-        }
+        //Form Actions//
+
+        $scope.submitForm = function() {
+            $scope.showSpinner = true;
+            supersonic.data.channel('localExercises').publish($localStorage.localExercises);
+            supersonic.ui.modal.hide();
+        };
 
         $scope.cancel = function () {
             supersonic.ui.modal.hide();
         };
 
     });
-
