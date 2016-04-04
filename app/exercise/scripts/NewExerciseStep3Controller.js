51f374ff4653d29481e2e6c264f3356f61269f6e
createadrln <ryan@createadrenaline.com>
2016-04-04 13:30:34 -0400

    refine form controller

--- app/exercise/scripts/NewExerciseStep3Controller.js
+++ app/exercise/scripts/NewExerciseStep3Controller.js
@@ -39,32 +39,47 @@ angular
 
         //Rep Series Settings and Functions//
 
-        if ($scope.exercise.reps == 'series') {
-            $scope.exercise.reps_series = {};
-            for(var set = 0; set < $scope.exercise.sets; set++ ) {
-                $scope.exercise.reps_series['reps_series' + '_' + set] = 1;
-            }
-        }
+        // if ($scope.exercise.reps == 'series') {
+        //     $scope.exercise.reps_series = {};
+        //     for(var set = 0; set < $scope.exercise.sets; set++ ) {
+        //         $scope.exercise.reps_series['reps_series' + '_' + set] = 1;
+        //     }
+        // }
 
         $scope.getCount = function(num) {
             return new Array(num);   
         };
 
-        $scope.increaseRepSeriesQty = function($index) {
-            $scope.exercise.reps_series['reps_series' + '_' + $index]++;
-        };
+        // $scope.increaseRepSeriesQty = function($index) {
+        //     $scope.exercise.reps_series['reps_series' + '_' + $index]++;
+        // };
+        //
+        // $scope.decreaseRepSeriesQty = function($index) {
+        //     $scope.exercise.reps_series['reps_series' + '_' + $index]++;
+        // };
 
-        $scope.decreaseRepSeriesQty = function($index) {
-            $scope.exercise.reps_series['reps_series' + '_' + $index]++;
+        //Exercise History//
+
+        $scope.exercise.history = {};
+        $scope.addToHistory = function () {
+            $scope.exercise.history = {
+                'history_date': new Date().toISOString(),
+                // 'sets' : exercise.setgoal,
+                // 'reps' : exercise.repgoal,
+                // 'maxweight' : exercise.maxweight,
+                // 'weight' : exercise.weight,
+                'weight_unit': $scope.exercise.weight_unit
+            };
         };
 
         //Form Actions//
 
         $scope.submitForm = function() {
-            $scope.showSpinner = true;
+            // $scope.showSpinner = true;
+            $scope.addToHistory();
             $localStorage.localExercises.push($scope.exercise);
-            supersonic.data.channel('localExercises').publish($localStorage.localExercises);
-            supersonic.ui.modal.hide();
+            // supersonic.data.channel('localExercises').publish($localStorage.localExercises);
+            // supersonic.ui.modal.hide();
         };
 
         $scope.cancel = function () {
