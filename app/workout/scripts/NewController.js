3e514853f972e1c6e9d3bda38558bd327e4ab62d
createadrln <ryan@createadrenaline.com>
2016-05-23 06:14:26 -0400

    App File Update

--- app/workout/scripts/NewController.js
+++ app/workout/scripts/NewController.js
@@ -11,36 +11,36 @@ angular
             $localStorage.localWorkouts = [];
         }
 
-        // if ($localStorage.localExercises) {
-        //     $scope.localExercises = $localStorage.localExercises;
-        //     $scope.exerciseSelectOptions = [];
-        //     for (var i=0; i<$scope.localExercises.length; i++) {
-        //         $scope.exerciseSelectOptions.push({
-        //             'id' : $scope.localExercises[i].id,
-        //             'name' : $scope.localExercises[i].name
-        //         });
-        //     }
-        //     $scope.exerciseSelects = [];
-        //     for (var selects = 0; selects < $scope.exercise_count; selects++) {
-        //         $scope.exerciseSelects.push({
-        //             'option' : $scope.exerciseSelectOptions
-        //         })
-        //     }
-        // }
-        //
-        // $scope.addExercise = function () {
-        //     $scope.exerciseSelects.push({
-        //         'option' : $scope.exerciseSelectOptions
-        //     });
-        //     $scope.exercise_count ++;
-        // };
-        //
-        // $scope.removeExercise = function () {
-        //     if ($scope.exercise_count > 1) {
-        //         $scope.exerciseSelects.pop();
-        //         $scope.exercise_count --;
-        //     }
-        // };
+        if ($localStorage.localExercises) {
+            $scope.localExercises = $localStorage.localExercises;
+            $scope.exerciseSelectOptions = [];
+            for (var i=0; i<$scope.localExercises.length; i++) {
+                $scope.exerciseSelectOptions.push({
+                    'id' : $scope.localExercises[i].id,
+                    'name' : $scope.localExercises[i].name
+                });
+            }
+            $scope.exerciseSelects = [];
+            for (var selects = 0; selects < $scope.exercise_count; selects++) {
+                $scope.exerciseSelects.push({
+                    'option' : $scope.exerciseSelectOptions
+                })
+            }
+        }
+
+        $scope.addExercise = function () {
+            $scope.exerciseSelects.push({
+                'option' : $scope.exerciseSelectOptions
+            });
+            $scope.exercise_count ++;
+        };
+
+        $scope.removeExercise = function () {
+            if ($scope.exercise_count > 1) {
+                $scope.exerciseSelects.pop();
+                $scope.exercise_count --;
+            }
+        };
 
         // Form Validate //
         $scope.validate = function () {
@@ -53,7 +53,13 @@ angular
                 $scope.showSpinner = true;
                 $localStorage.localWorkouts.push($scope.workout);
                 supersonic.data.channel('localWorkouts').publish($localStorage.localWorkouts);
-                supersonic.ui.modal.hide();
+                if (steroids.view.params.location == 'onboarding') {
+                    alert($scope.exercise.location);
+                    var workoutView = new supersonic.ui.View('workout#index');
+                    supersonic.ui.layers.push(workoutView);
+                } else {
+                    supersonic.ui.modal.hide();
+                }
             }
         };
 
